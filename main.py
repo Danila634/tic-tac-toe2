f267de30187ab36ce6403714496ef38c0eb1f601
Danila634 <sestakovdanila9@gmail.com>
2023-11-22 16:16:12 +0500

    add color output using colorama

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import colorama
 import os
 
 
 def clear():
     os.system('cls')
+colorama.init()
+
+blue_color = colorama.Fore.BLUE
+black_color = colorama.Fore.BLACK
+cyan_color = colorama.Fore.CYAN
+yellow_color = colorama.Fore.YELLOW
+green_color = colorama.Fore.GREEN
+white_color = colorama.Fore.WHITE
+red_color = colorama.Fore.RED
 
 
 board = list(range(0, 9))
@@ -17,30 +27,30 @@ win_coords = (
     (0, 4, 8), (2, 4, 6)
 )
 player_token = ''
-print('Tic-tac toe the game for two players. ')
+print(f'{green_color}Tic-tac toe the game for two players. ')
 
 while not is_win:
     for i in range(board_size):
         print(' ' * spaces, end='')
         print('-' * dashes)
         print(' ' * spaces, end='')
-        print(f'| {board[0 + i * 3]} | {board[1 + i * 3]} | {board[2 + i * 3]} |')
+        print(f'{blue_color}| {board[0 + i * 3]} |{white_color} {board[1 + i * 3]} |{red_color} {board[2 + i * 3]} |')
         print(' ' * spaces, end='')
         print('-' * dashes)
     if turn_counter % 2 == 0:
         player_token = 'X'
     else:
         player_token = 'O'
-    player_answer = input(f'Where we put a {player_token}?: ')
+    player_answer = input(f'{cyan_color}Where we put a {player_token}?: ')
     try:
         player_answer = int(player_answer)
     except ValueError:
-        print('Неправильный ввод. Нужно ввести число.')
+        print(f'{red_color}Неправильный ввод. Нужно ввести число.')
         continue
     try:
         board_value = board[player_answer]
     except IndexError:
-        print('Неправильный ввод. Должны быть числа от 0 до 8 включительно.')
+        print(f'{red_color}Неправильный ввод. Должны быть числа от 0 до 8 включительно.')
         continue
     if str(board[player_answer]) not in 'XO':
         board[player_answer] = player_token
@@ -59,13 +69,13 @@ while not is_win:
                 print(' ' * spaces, end='')
                 print('-' * dashes)
                 print(' ' * spaces, end='')
-                print(f'| {board[0 + i * 3]} | {board[1 + i * 3]} | {board[2 + i * 3]} |')
+                print(f'{green_color}| {board[0 + i * 3]} |{green_color} {board[1 + i * 3]} |{green_color} {board[2 + i * 3]} |')
                 print(' ' * spaces, end='')
                 print('-' * dashes)
             print(f'{player_token} is win')
             break
     if turn_counter == '9':
-        print('Draw! Your amazing!')
+        print(f'{green_color}Draw! Your amazing!')
         break
-exit_confirmation = input('Нажми Enter для выхода.')
+exit_confirmation = input(f'{cyan_color}Нажми Enter для выхода.')
 quit()
